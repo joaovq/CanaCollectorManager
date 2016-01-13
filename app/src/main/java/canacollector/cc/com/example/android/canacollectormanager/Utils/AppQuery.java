c76d7aa04ac74c97cbc587cf931960d3223ef545
Breno <breno.dealmeida@gmail.com>
2016-01-13 18:06:32 -0200

    Novas queries

--- app/src/main/java/canacollector/cc/com/example/android/canacollectormanager/Utils/AppQuery.java
+++ app/src/main/java/canacollector/cc/com/example/android/canacollectormanager/Utils/AppQuery.java
@@ -1,5 +1,7 @@
 package canacollector.cc.com.example.android.canacollectormanager.Utils;
 
+import android.util.Log;
+
 import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -7,6 +9,8 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.util.List;
+
+import canacollector.cc.com.example.android.canacollectormanager.View.Model.Tonel;
 
 /**
  * Created by Breno on 1/13/2016.
@@ -21,31 +25,46 @@ public class AppQuery {
     private static void setEstoqueTotalFromServer() {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Tonel");
         query.whereEqualTo("alambique", AppUtils.getAlambique());
+        query.whereGreaterThan("estoque", 0.0);
 
         query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(final List<ParseObject> scoreList, ParseException e) {
+            public void done(final List<ParseObject> tonelList, ParseException e) {
                 if (e != null) {
-                    // There was an error or the network wasn't available.
+                    Log.e("AppQuery", e.toString());
                     return;
                 }
 
+                Double total = 0.0;
+                Tonel tonelResult = new Tonel();
+                for (ParseObject parseObject : tonelList) {
+                    tonelResult = (Tonel) parseObject;
+                    total += tonelResult.getEstoque();
+                }
+
+                tonelResult.setName("Temp");
+                tonelResult.setEstoque(total);
+
+                tonelList.clear();
+                tonelList.add(tonelResult);
                 // Release any objects previously pinned for this query.
-                ParseObject.unpinAllInBackground(ESTOQUE_TOTAL, scoreList, new DeleteCallback() {
+                ParseObject.unpinAllInBackground(ESTOQUE_TOTAL, tonelList, new DeleteCallback() {
                     public void done(ParseException e) {
                         if (e != null) {
-                            // There was some error.
+                            Log.e("AppQuery", e.toString());
                             return;
                         }
 
                         // Add the latest results for this query to the cache.
-                        ParseObject.pinAllInBackground(ESTOQUE_TOTAL, scoreList);
+                        ParseObject.pinAllInBackground(ESTOQUE_TOTAL, tonelList);
                     }
                 });
             }
         });
     }
 
-   // private static Double getAreaTotal() {}
+//    private static Double getAreaTotal() {
+//
+//    }
 
    // private static Double getProducaoMedia() {
 
